5d905cf4e3dbfee4877e6e8be4960cb383200cc6
unknown <hisham.saeed@kade-gmbh.de>
2025-11-06 13:13:26 +0100

    added logo and add book section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import './App.css'
+import myLogo from './assets/svg/logo.svg';
+import add from './assets/svg/add.svg';
 
-// test protection rule
 export default function App() {
   return (
-    <main style={{ padding: 24 }}>
-      <h1>📚 My Bookstore</h1>
-      <nav style={{ display: 'flex', gap: 12 }}>
-        <Link to="/books">Books</Link>
-        <Link to="/books/new">Add Book</Link>
-      </nav>
-    </main>
+    <div>
+      <header>
+        <div className='logo'>
+          <div className='ImageContainer'>
+            <img className='logoImage' src={myLogo} alt="Logo"/>
+          </div>
+          <label className='logoLabel'>BookLibrary</label>
+        </div>
+        <div className='navigation'>
+          <div>Dashboard</div>
+          <div>Add Book</div>
+        </div>
+      </header>
+      <main>
+        <div className='addBooksSection'>
+          <div className='addBooksTitle'>
+            <h2>My Books</h2>
+            <p>Browse and manage your personal library connection</p>
+          </div>
+          <div className='button'>
+            <img className='addImage' src={add} alt="add"/>
+            <label>Add New Book</label>
+          </div>
+        </div>
+      </main>
+    </div>
+    // <main style={{ padding: 24 }}>
+    //   <h1>📚 My Bookstore</h1>
+    //   <nav style={{ display: 'flex', gap: 12 }}>
+    //     <Link to="/books">Books</Link>
+    //     <Link to="/books/new">Add Book</Link>
+    //   </nav>
+    // </main>
   )
 }
