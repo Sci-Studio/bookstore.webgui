7e205188b0910fe3035f3aa781a63f3a262e7591
HishamSaeed <hisham.saeed.92@gmail.com>
2025-11-12 12:14:18 +0100

    moved header to heade component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useNavigate, NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './App.css'
-import myLogo from './assets/svg/logo.svg';
 import add from './assets/svg/add.svg';
+import HeaderBar from './components/header-bar/HeaderBar'
 import BooksList from './components/books-list/BooksList';
 
 export default function App() {
@@ -10,20 +10,7 @@ export default function App() {
 
   return (
     <div>
-      <header className='header'>
-        <div className='headerContainer'>
-          <div className='logo'>
-            <div className='ImageContainer'>
-              <img className='logoImage' src={myLogo} alt="Logo"/>
-            </div>
-            <label className='logoLabel'>BookLibrary</label>
-          </div>
-          <div className='navigation'>
-            <NavLink to="/" className={({ isActive }) => isActive ? 'active-link' : ''}>Dashboard</NavLink>
-            <NavLink to="/books/new" className={({ isActive }) => isActive ? 'active-link' : ''}>Add Book</NavLink>
-          </div>
-        </div>
-      </header>
+      <HeaderBar></HeaderBar>
       <main className='mainContainer'>
         <div className='addBooksSection'>
           <div className='addBooksTitle'>
---
+++ src/components/header-bar/HeaderBar.tsx
@@ -0,0 +1,43 @@
+import styles from './HeaderBar.module.css';
+import { NavLink } from 'react-router-dom'
+import myLogo from '../../assets/svg/logo.svg';
+
+interface INavigationItem {
+    name: string;
+    link: string;
+}
+
+export default function HeaderBar() {
+
+    const navigationList: Array<INavigationItem> = [
+        { name: 'Dashboard', link: '/' },
+        { name: 'Add Book', link: '/books/new' }
+    ];
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.headerContainer}>
+                <div className={styles.main}>
+                    <div className={styles.logo}>
+                        <div className={styles.ImageContainer}>
+                            <img className={styles.logoImage} src={myLogo} alt="Logo"/>
+                        </div>
+                        <span style={{width: '12px'}}></span>
+                        <h1 className={styles.logoLabel}>BookLibrary</h1>
+                    </div>
+                    <div className={styles.navigation}>
+                        {navigationList.map((navItem) => (
+                            <div className={styles.navContainer}>
+                                <NavLink 
+                                    to={navItem.link} 
+                                    className={({ isActive }) => isActive ? styles.activeLink : ''}>
+                                        {navItem.name}
+                                </NavLink>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </header> 
+    )
+}
