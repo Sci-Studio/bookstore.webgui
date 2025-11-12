7d60a887dfe6341fafba0a5b834e19ef76538f33
HishamSaeed <hisham.saeed.92@gmail.com>
2025-11-12 14:26:49 +0100

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-import { useNavigate } from 'react-router-dom'
 import './App.css'
-import add from './assets/svg/add.svg';
 import HeaderBar from './components/header-bar/HeaderBar'
 import HomePage from './pages/home-page/HomePage'
-import BooksList from './components/books-list/BooksList';
 
 export default function App() {
-
-  const navigate = useNavigate()
 
   return (
     <div className='appContainer'>
       <HeaderBar></HeaderBar>
       <HomePage></HomePage>
-      {/* <main className='mainContainer'>
-        <div className='addBooksSection'>
-          <div className='addBooksTitle'>
-            <h2>My Books</h2>
-            <p>Browse and manage your personal library connection</p>
-          </div>
-          <div className='button' onClick={() => navigate('/books/new')}>
-            <img className='addImage' src={add} alt="add"/>
-            <label>Add New Book</label>
-          </div>
-        </div>
-        <div className='searchSection'>
-
-        </div>
-        <div className='bookListSection'>
-          <BooksList></BooksList>
-        </div>
-      </main> */}
     </div>
   )
 }
