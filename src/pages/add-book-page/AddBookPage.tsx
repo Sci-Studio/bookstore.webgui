9919f4f65961a2191517cbc60d74812e4b173620
HishamSaeed <hisham.saeed.92@gmail.com>
2025-11-14 16:57:37 +0100

    added push buttons

--- src/pages/add-book-page/AddBookPage.tsx
+++ src/pages/add-book-page/AddBookPage.tsx
@@ -1,6 +1,53 @@
-import styles from './AddBookPage.module.css'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import styles from './AddBookPage.module.css';
+import PushButton from '../../widgets/push-button/PushButton';
+import add from '../../assets/svg/add.svg';
+import close from '../../assets/svg/close.svg';
+import axios from 'axios'
+
+const API_BASE = import.meta.env.VITE_API_BASE || '/api'
 
 export default function AddBookPage() {
+
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [price, setPrice] = useState('');
+    const [imageFile, setImageFile] = useState<File | null>(null);
+    const [msg, setMsg] = useState('');
+    const [uploading, setUploading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const submit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setMsg('')
+        setUploading(true)
+        let imageUrl = ''
+        try {
+            
+
+            if (imageFile) {
+                const formData = new FormData()
+                formData.append('image', imageFile)
+
+                const uploadRes = await axios.post(`${API_BASE}/upload`, formData, {
+                    headers: { 'Content-Type': 'multipart/form-data' },
+                })
+
+                imageUrl = uploadRes.data.imageUrl
+            }
+        } catch (err) {
+            console.error(err)
+            setMsg('❌ Failed to save book')
+        } finally {
+            setUploading(false)
+        }
+        const priceCents = Math.round(parseFloat(price) * 100)
+        await axios.post(`${API_BASE}/books`, { title, author, priceCents, imageUrl })
+        setMsg('Saved!')
+        setTitle(''); setAuthor(''); setPrice(''); setImageFile(null)
+    }
 
     return (
         <main className={styles.main}>
@@ -12,7 +59,11 @@ export default function AddBookPage() {
                 <form className={styles.formContainer}>
                     <div className={styles.bookParameter}></div>
                     <div className={styles.bookImage}></div>
-                    <div className={styles.formControls}></div>
+                    <div className={styles.formControls}>
+                        <PushButton imageUrl={close} text='Cancel' className={styles.cancelButton} onClick={() => navigate('/')}></PushButton>
+                        <PushButton imageUrl={add} text={uploading ? 'Uploading...' : 'Add Book to Library'} className={styles.addButton} onClick={() => submit}></PushButton>
+                    </div>
+                    <div>{msg}</div>
                 </form>
             </div>
             <div className={styles.recentBooks}></div>
